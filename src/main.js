aac22b460f2db7b23a3e63c94aadb1c56ebfb5c0
Ebenezer Oladutemu <olakennyfemi@gmail.com>
2020-02-25 19:15:31 +0100

    Update main js

--- src/main.js
+++ src/main.js
@@ -10,21 +10,8 @@ Vue.use(Cheeksy);
 
 const router = new Cheeksy({
   routes,
-  mode: 'history',
-  scrollBehavior(to, from, savedPostion){
-    if (to.hash) {
-      return {selector: to.hash}
-    }
-    // return {x: 0, y: 600};
-  }
-  
+  mode: 'history'
 });
-  router.beforeEach ((to, from, next) => {
-    console.log('Global beforeEach')
-    next(); //next needs to be called or app won't go the next route
-            //alternatively, parse false to cancel the operation and stay on same page
-            //you can also parse the name of the router-link you want it to go
-  })
 
 new Vue({
   el: '#app',
