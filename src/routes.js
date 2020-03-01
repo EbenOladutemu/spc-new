0736ab26850377138aa9e4dbdbd3f953527cdcbf
Ebenezer Oladutemu <olakennyfemi@gmail.com>
2020-03-01 10:08:43 +0100

    Comment routes giving error (have no freaking idea why)

--- src/routes.js
+++ src/routes.js
@@ -27,11 +27,11 @@ const Gallery = resolve => {
   })
 }
 
-const Sermon = resolve => {
-  require.ensure(['./components/pages/Sermon'], () => {
-    resolve(require('./components/pages/Sermon'));
-  })
-}
+// const Sermon = resolve => {
+//   require.ensure(['./components/pages/Sermon'], () => {
+//     resolve(require('./components/pages/Sermon'));
+//   })
+// }
 
 const Donate = resolve => {
   require.ensure(['./components/pages/Donate'], () => {
@@ -39,29 +39,29 @@ const Donate = resolve => {
   })
 }
 
-const About = resolve => {
-  require.ensure(['./components/pages/church/About'], () => {
-    resolve(require('./components/pages/church/About'));
-  })
-}
+// const About = resolve => {
+//   require.ensure(['./components/pages/church/About'], () => {
+//     resolve(require('./components/pages/church/About'));
+//   })
+// }
 
-const Leadership = resolve => {
-  require.ensure(['./components/pages/church/Leadership'], () => {
-    resolve(require('./components/pages/church/Leadership'));
-  })
-}
+// const Leadership = resolve => {
+//   require.ensure(['./components/pages/church/Leadership'], () => {
+//     resolve(require('./components/pages/church/Leadership'));
+//   })
+// }
 
-const Mission = resolve => {
-  require.ensure(['./components/pages/church/Mission'], () => {
-    resolve(require('./components/pages/church/Mission'));
-  })
-}
+// const Mission = resolve => {
+//   require.ensure(['./components/pages/church/Mission'], () => {
+//     resolve(require('./components/pages/church/Mission'));
+//   })
+// }
 
-const Vision = resolve => {
-  require.ensure(['./components/pages/church/Vision'], () => {
-    resolve(require('./components/pages/church/Vision'));
-  })
-}
+// const Vision = resolve => {
+//   require.ensure(['./components/pages/church/Vision'], () => {
+//     resolve(require('./components/pages/church/Vision'));
+//   })
+// }
 
 const WorshipTimes = resolve => {
   require.ensure(['./components/pages/church/WorshipTimes'], () => {
@@ -75,11 +75,11 @@ export const routes = [
   { path: '/contact', component: Contact },
   { path: '/donate', component: Donate, name: 'donate' },
   { path: '/gallery', component: Gallery, name: 'gallery' },
-  { path: '/sermon', component: Sermon, name: 'sermon' },
-  { path: '/church/about', component: About },
-  { path: '/church/leadership', component: Leadership },
-  { path: '/church/mission', component: Mission },
-  { path: '/church/vision', component: Vision },
+  // { path: '/sermon', component: Sermon, name: 'sermon' },
+  // { path: '/church/about', component: About },
+  // { path: '/church/leadership', component: Leadership },
+  // { path: '/church/mission', component: Mission },
+  // { path: '/church/vision', component: Vision },
   { path: '/church/worship-times', component: WorshipTimes },
   // { path: '/*', redirect: {name: 'home'} }
 ]
